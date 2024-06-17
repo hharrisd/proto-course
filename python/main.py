06b85d03891bc38595b0806a0313cac4a5bf912c
Humberto Harris <hharris@ekumenlabs.com>
2024-06-17 10:59:27 -0500

    Complex messages

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import proto.simple_pb2 as simple_pb2
+import proto.complex_pb2 as complex_pb2
 
 
 def simple():
@@ -10,5 +11,19 @@ def simple():
     )
 
 
+def complex():
+    message = complex_pb2.Complex()
+    message.one_dummy.id = 42
+    message.one_dummy.name = "Loki"
+
+    message.multiple_dummies.add(id=43, name="Jira")
+    message.multiple_dummies.add(id=44, name="Jenny")
+    message.multiple_dummies.add(id=45, name="Axel")
+
+    return message
+
 if __name__ == "__main__":
+    print("Simple message:")
     print(simple())
+    print('Complex message:')
+    print(complex())
